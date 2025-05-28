5606dc0e8e8ea49ec578a806200b414998b6269d
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-05-28 16:48:18 -0600

    CharacterSetUp-0.4

---
+++ Source/ProyectHades/Private/WarriorGamplayTags.cpp
@@ -0,0 +1,10 @@
+// KasaiRaito Studios All Rights Reserved
+
+#include "WarriorGamplayTags.h"
+
+namespace  WarriorGameplayTags
+{
+	/** INPUT TAGS **/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "Input.Move" )
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "Input.Look")
+}
---
+++ Source/ProyectHades/Public/DataAssets/Input/DataAsset_InputConfig.h
@@ -0,0 +1,23 @@
+// KasaiRaito Studios All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_InputConfig.generated.h"
+
+USTRUCT(BlueprintType)
+struct FWarriorInputActionConfig
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+UCLASS()
+class PROYECTHADES_API UDataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};

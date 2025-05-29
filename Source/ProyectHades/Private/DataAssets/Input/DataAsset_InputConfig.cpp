5c555de6e26b0f54293e35cebae18437ad87d585
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-05-28 23:01:04 -0600

    CharacterSetUp-0.5

--- Source/ProyectHades/Private/DataAssets/Input/DataAsset_InputConfig.cpp
+++ Source/ProyectHades/Private/DataAssets/Input/DataAsset_InputConfig.cpp
@@ -3,3 +3,15 @@
 
 #include "DataAssets/Input/DataAsset_InputConfig.h"
 
+UInputAction* UDataAsset_InputConfig::FidNativeInputActionByTag(const FGameplayTag& InInputTag)
+{
+	for (const FWarriorInputActionConfig& InputActionConfig : NativeInputActions)
+	{
+		if (InputActionConfig.InputTag == InInputTag && InputActionConfig.InputAction)
+		{
+			return InputActionConfig.InputAction;
+		}
+	}
+	
+	return nullptr;
+}
--- Source/ProyectHades/ProyectHades.Build.cs
+++ Source/ProyectHades/ProyectHades.Build.cs
@@ -14,8 +14,8 @@ public class ProyectHades : ModuleRules
 			"CoreUObject", 
 			"Engine", 
 			"InputCore", 
-			"EnhancedInput",
 			"GameplayTags",
+			"EnhancedInput",
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });

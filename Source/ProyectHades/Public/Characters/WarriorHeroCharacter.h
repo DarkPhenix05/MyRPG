57a2d0a9be8751ed1dc74474947d4b1a7cc7f73d
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-05-29 00:16:09 -0600

    TerminatorFix

---
+++ Source/ProyectHades/Public/Characters/WarriorHeroCharacter.h
@@ -0,0 +1,33 @@
+// KasaiRaito Studios All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/WarriorBaseCharacter.h"
+#include "WarriorHeroCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+/**
+ * 
+ */
+UCLASS()
+class PROYECTHADES_API AWarriorHeroCharacter : public AWarriorBaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AWarriorHeroCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+#pragma region Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FolowCamera;
+#pragma endregion
+};

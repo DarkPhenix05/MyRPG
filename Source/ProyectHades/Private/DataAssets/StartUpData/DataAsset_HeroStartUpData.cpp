a77238de37102f37d37134d19279f7e3f05ccf0d
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-01 21:34:33 -0600

    MeleeComboSystem - 1.7

--- Source/ProyectHades/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ Source/ProyectHades/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -2,4 +2,31 @@
 
 
 #include "DataAssets/StartUpData/DataAsset_HeroStartUpData.h"
+#include "AbiilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbiilitySystem/WarriorAbilitySystemComponent.h"
 
+bool FWarriorHeroAbilitySet::IsValid() const
+{
+	return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive,
+	int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+
+	for (const FWarriorHeroAbilitySet& AbilitySet : HeroStartAbilitySets)
+	{
+		if (AbilitySet.IsValid())
+		{
+			continue;
+		}
+
+		FGameplayAbilitySpec AbilitySpec(AbilitySet.AbilityToGrant);
+		AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+		AbilitySpec.DynamicAbilityTags.AddTag(AbilitySet.InputTag);
+		
+		InASCToGive->GiveAbility(AbilitySpec);
+	}
+}

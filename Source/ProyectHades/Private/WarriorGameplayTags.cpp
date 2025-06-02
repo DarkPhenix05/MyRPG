9ec30e28f28bf5c5232b712910e74f574c5204a7
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-01 23:53:54 -0600

    MeleeComboSystem- 1.10

--- Source/ProyectHades/Private/WarriorGameplayTags.cpp
+++ Source/ProyectHades/Private/WarriorGameplayTags.cpp
@@ -11,5 +11,8 @@ namespace  WarriorGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_UnequipAxe, "Input.UnequipAxe");
 
 	/** Player TAGS **/
-	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe, "Player.Weapon.Axe" );
+	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe, "Player.Weapon.Axe");
+	
+	UE_DEFINE_GAMEPLAY_TAG(Player_Events_Equip_Axe, "Player.Event.Equip.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Events_Unequip_Axe, "Player.Event.Unequip.Axe");
 }

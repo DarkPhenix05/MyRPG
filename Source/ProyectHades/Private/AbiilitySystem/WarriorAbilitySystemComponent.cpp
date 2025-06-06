29b696cff320ae572f1a1f2ddfba81fe73867209
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-05 21:38:23 -0600

    MeleeComboSystem-1.16

--- Source/ProyectHades/Private/AbiilitySystem/WarriorAbilitySystemComponent.cpp
+++ Source/ProyectHades/Private/AbiilitySystem/WarriorAbilitySystemComponent.cpp
@@ -53,3 +53,22 @@ void UWarriorAbilitySystemComponent::GrantHeroWeaponAbilities(
 		OutGrantedAbilitySpecHandles.AddUnique(GiveAbility(AbilitySpec));
 	}
 }
+
+void UWarriorAbilitySystemComponent::RemoveGrantedHeroWEaponAbilities(
+	TArray<FGameplayAbilitySpecHandle>& InSpecHandlesToRemove)
+{
+	if (InSpecHandlesToRemove.IsEmpty())
+	{
+		return;
+	}
+
+	for(const FGameplayAbilitySpecHandle& SpecHandle : InSpecHandlesToRemove)
+	{
+		if (SpecHandle.IsValid())
+		{
+			ClearAbility(SpecHandle);
+		}
+
+		InSpecHandlesToRemove.Empty();
+	}
+}

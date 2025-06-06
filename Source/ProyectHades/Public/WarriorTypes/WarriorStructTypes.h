29a318231cd96b7a6a95ecd46a2572e679ceeb0e
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-05 18:28:55 -0600

    MeleeComboSystem-1.13

---
+++ Source/ProyectHades/Public/WarriorTypes/WarriorStructTypes.h
@@ -0,0 +1,17 @@
+// KasaiRaito Studios All Rights Reserved
+
+#pragma once
+#include "WarriorStructTypes.generated.h"\
+
+class UWarriorHeroLinkedAnimLayer;
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FWarriorHeroWeaponData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UWarriorHeroLinkedAnimLayer> WeaponAnimLayerToLink;
+};

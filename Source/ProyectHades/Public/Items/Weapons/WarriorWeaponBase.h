e373cbe3c0573b86c67186cc4a439ba142cabd16
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-11 07:06:51 -0600

    HeroCombat - 2.13

--- Source/ProyectHades/Public/Items/Weapons/WarriorWeaponBase.h
+++ Source/ProyectHades/Public/Items/Weapons/WarriorWeaponBase.h
@@ -23,6 +23,12 @@ protected:
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = "Weapons")
 	UBoxComponent* WeaponCollisionBox;
 
+	UFUNCTION()
+	virtual void OnCollisionBoXBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnCollisionBoXEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
 public:
 	FORCEINLINE UBoxComponent* GetWeaponCollisionBox() const { return WeaponCollisionBox; }
 };

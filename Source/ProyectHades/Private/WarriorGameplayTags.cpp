4b195e135ffe7b6a10c0244b0f6bb38137380fc0
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-12 21:12:23 -0600

    HeroCombat - 2.25

--- Source/ProyectHades/Private/WarriorGameplayTags.cpp
+++ Source/ProyectHades/Private/WarriorGameplayTags.cpp
@@ -33,8 +33,14 @@ namespace  WarriorGameplayTags
 
 	/** Enemy TAGS **/
 	UE_DEFINE_GAMEPLAY_TAG(Enemy_Weapon, "Enemy.Weapon")
+	UE_DEFINE_GAMEPLAY_TAG(Enemy_Ability_Melee, "Enemy.Ability.Melee")
+	UE_DEFINE_GAMEPLAY_TAG(Enemy_Ability_Ranged, "Enemy.Ability.Ranged")
 
 	/** Shared TAGS **/
+	UE_DEFINE_GAMEPLAY_TAG(Shared_Ability_HitReact, "Shared.Ability.HitReact")
+	
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_MeleeHit, "Shared.Event.MeleeHit")
+	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_HitReact, "Shared.Event.HitReact")
+	
 	UE_DEFINE_GAMEPLAY_TAG(Shared_SetByCaller_BaseDamage, "Shared.SetByCaller.BaseDamage")
 }

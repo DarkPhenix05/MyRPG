623809dadf6199fe3a4e30d320cc7f4c348a4d2d
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-14 04:40:29 -0600

    EnemyAI - 3.4

--- Source/ProyectHades/Public/Controllers/WarriorAIController.h
+++ Source/ProyectHades/Public/Controllers/WarriorAIController.h
@@ -6,6 +6,8 @@
 #include "AIController.h"
 #include "WarriorAIController.generated.h"
 
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
 /**
  * 
  */
@@ -15,5 +17,15 @@ class PROYECTHADES_API AWarriorAIController : public AAIController
 	GENERATED_BODY()
 
 public:
-	AWarriorAIController(const FObjectInitializer& ObjectInitializer);	
+	AWarriorAIController(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAIPerceptionComponent* EnemyPerceptionComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAISenseConfig_Sight* AISenseConfig_Sight;
+
+	UFUNCTION()
+	virtual void OneEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
 };

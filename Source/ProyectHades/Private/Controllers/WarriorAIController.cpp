4564dc219269133534bea436c45537d8dbe6b3da
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-14 05:29:08 -0600

    EnemyAI - 3.6

--- Source/ProyectHades/Private/Controllers/WarriorAIController.cpp
+++ Source/ProyectHades/Private/Controllers/WarriorAIController.cpp
@@ -5,6 +5,7 @@
 #include "Navigation/CrowdFollowingComponent.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 #include "WarriorDebugHelper.h"
 
@@ -49,6 +50,9 @@ void AWarriorAIController::OneEnemyPerceptionUpdated(AActor* Actor, FAIStimulus 
 {
 	if (Stimulus.WasSuccessfullySensed() && Actor)
 	{
-		Debug::Print(Actor->GetActorNameOrLabel()+ TEXT(" was sensed"), FColor::Green);
+		if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+		{
+			BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+		}
 	}
 }

36aab4bd028cd9c168b59a6e4b81da1f22ca0846
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-14 04:25:49 -0600

    Enemy AI - 3.3

---
+++ Source/ProyectHades/Public/Controllers/WarriorAIController.h
@@ -0,0 +1,19 @@
+// KasaiRaito Studios All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "WarriorAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROYECTHADES_API AWarriorAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AWarriorAIController(const FObjectInitializer& ObjectInitializer);	
+};

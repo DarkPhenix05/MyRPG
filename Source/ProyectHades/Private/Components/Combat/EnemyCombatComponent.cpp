f5018d0ba8c49b53c5776d356843fe6b4f3d3448
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-14 18:07:04 -0600

    EnemyAI-3.21

--- Source/ProyectHades/Private/Components/Combat/EnemyCombatComponent.cpp
+++ Source/ProyectHades/Private/Components/Combat/EnemyCombatComponent.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Components/Combat/EnemyCombatComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "WarriorGameplayTags.h"
 
 #include "WarriorDebugHelper.h"
 
@@ -9,7 +11,42 @@ void UEnemyCombatComponent::OnHitTargetActor(AActor* HitActor)
 {
 	if (HitActor)
 	{
-		Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is Hitting") + HitActor->GetActorNameOrLabel());
+		//Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" is Hitting") + HitActor->GetActorNameOrLabel());
+		if (OverlappingActors.Contains(HitActor))
+		{
+			return;
+		}
+
+		OverlappingActors.AddUnique(HitActor);
+
+		//TO DO:: implement Block check
+		bool bIsValidBlock = false;
+
+		const bool bIsPLayerBlocking = false;
+		const bool bIsMyAttackUnblocable = false;
+
+		if (bIsPLayerBlocking && !bIsMyAttackUnblocable)
+		{
+			//TO DO:: Handle successful block
+		}
+
+		FGameplayEventData EventData;
+		EventData.Instigator = GetOwningPawn();
+		EventData.Target = HitActor;
+		
+		if (bIsValidBlock)
+		{
+			
+		}
+		else
+		{
+			UAbilitySystemBlueprintLibrary::SendGameplayEventToActor
+			(
+				GetOwningPawn(),
+				WarriorGameplayTags::Shared_Event_MeleeHit,
+				EventData	
+			);
+		}
 	}
 	
 }

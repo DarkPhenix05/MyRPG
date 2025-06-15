84e9ea09bf6ab3a855a210b2019d70d20109326f
DarkPhoenix <152333387+DarkPhenix05@users.noreply.github.com>
2025-06-14 19:06:04 -0600

    EnemyAI-3.22

--- Source/ProyectHades/Private/AbiilitySystem/Abilities/WarriorEnemyGameplayAbility.cpp
+++ Source/ProyectHades/Private/AbiilitySystem/Abilities/WarriorEnemyGameplayAbility.cpp
@@ -2,7 +2,10 @@
 
 
 #include "AbiilitySystem/Abilities/WarriorEnemyGameplayAbility.h"
+
 #include "Characters/WarriorEnemyCharacter.h"
+#include "AbiilitySystem/WarriorAbilitySystemComponent.h"
+#include "WarriorGameplayTags.h"
 
 AWarriorEnemyCharacter* UWarriorEnemyGameplayAbility::GetEnemyCharacterFromActorInfo()
 {
@@ -16,4 +19,28 @@ AWarriorEnemyCharacter* UWarriorEnemyGameplayAbility::GetEnemyCharacterFromActor
 UEnemyCombatComponent* UWarriorEnemyGameplayAbility::GetEnemyCombatComponentFromActorInfo()
 {
 	return GetEnemyCharacterFromActorInfo()->GetEnemyCombatComponent();
+}
+
+FGameplayEffectSpecHandle UWarriorEnemyGameplayAbility::MakeEnemyDamageEffectSpecHandle(
+	TSubclassOf<UGameplayEffect> EffectClass, const FScalableFloat& InDamageScalableFloat)
+{
+	check(EffectClass)
+	FGameplayEffectContextHandle ContextHandle = GetWarriorAbilitySystemComponentFromActorInfo()->MakeEffectContext();
+	ContextHandle.SetAbility(this);
+	ContextHandle.AddSourceObject(GetAvatarActorFromActorInfo());
+	ContextHandle.AddInstigator(GetAvatarActorFromActorInfo(), GetAvatarActorFromActorInfo());
+
+	FGameplayEffectSpecHandle EffectSpecHandle = GetWarriorAbilitySystemComponentFromActorInfo()->MakeOutgoingSpec
+	(
+		EffectClass,
+		GetAbilityLevel(),
+		ContextHandle
+	);
+
+	EffectSpecHandle.Data->SetSetByCallerMagnitude(
+		WarriorGameplayTags::Shared_SetByCaller_BaseDamage,
+		InDamageScalableFloat.GetValueAtLevel(GetAbilityLevel())
+	);
+	
+	return EffectSpecHandle;
 }
